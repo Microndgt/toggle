fc97d533d2d2e30eb28f5602c58527c9d845e3f0
Microndgt <dgt_x@foxmail.com>
2017-07-02 10:42:05 +0800

    update and generate & readme

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
+Contents
+===
+
+- [Usage](https://github.com/Microndgt/toggle#usage)
+  - [QuickStart](https://github.com/Microndgt/toggle#quickstart)
+  - [arguments of Toggle](https://github.com/Microndgt/toggle#arguments-of-toggle)
+- [Author](https://github.com/Microndgt/toggle#author)
+
 # toggle
-generate markdown table of contents on the top of md file
+generate and update markdown table of contents on the top of md file
 
 ![](https://img.shields.io/badge/Python-3.5-green.svg)
 
+Usage
+=====
+
+QuickStart
+---
+
+```
+t = Toggle(filename="path/to/file", url_prefix="content/url/prefix")
+# 生成目录
+t.generate()  # return contents of md
+# 更新目录
+t.update()  # return contents of md
+```
+
+arguments of Toggle
+-------------------
+
+- url_prefix: 该md显示网页的url
+- filename: 输入为文件路径
+- content: 输入为文件内容数据,list,每一项为文件数据的一行,若同时输入文件名和文件数据,以文件数据优先
+- match_rules: 匹配规则,dict,默认匹配 === 和 ---,值为该匹配项对应生成的md目录行的前缀,如===对应`- `
+- sub_rules: 替换规则,dict以替换标题项中的字符,使得符合markdown解析规则,比如替换'.'为''
+
+Example
+=======
+
+本文就是自动生成目录的一个例子
 
 Author
 ======
--- toggle.py
+++ toggle.py
@@ -5,15 +5,27 @@ class ContentErrorException(Exception):
     pass
 
 
+def next_no_blank(contents):
+    for index, single_line in enumerate(contents):
+        if single_line.strip():
+            return index
+
+
+def next_blank(contents):
+    for index, single_line in enumerate(contents):
+        if not single_line.strip():
+            return index
+
+
 class Toggle(object):
     def __init__(self, url_prefix, content=None, filename=None, match_rules=None, sub_rules=None):
         if not content and not filename:
             raise ContentErrorException("Please pass in some content to transfer")
-        elif filename is not None:
+        elif content is not None:
+            self.content = content
+        else:
             self.filename = filename
             self.content = open(filename).readlines()
-        else:
-            self.content = content
 
         self.url_prefix = url_prefix
         self.sub_rules = sub_rules or {
@@ -28,11 +40,9 @@ class Toggle(object):
         }
         self.default_start = ["Contents\n", "===\n", "\n"]
 
-    def parse(self, if_update=False):
+    def parse(self, contents):
         _last = None
-        for line in self.content:
-            if not if_update and line.startswith(("contents", "Contents")):
-                raise ContentErrorException("already has a content")
+        for line in contents:
             if line.startswith(tuple(self.match_rules.keys())):
                 # 匹配到了,但是没有_last或者_last为空则报错
                 if not _last or not _last.strip():
@@ -44,39 +54,62 @@ class Toggle(object):
         origin_line = line
         for match, repl in self.sub_rules.items():
             line = re.sub(match, eval(repl) if repl.startswith("lambda") else repl, line)
-        url_line = "[{}]({})".format(origin_line.strip(), self.url_prefix + "#" + line.strip())
+        url_line = "[{}]({})\n".format(origin_line.strip(), self.url_prefix + "#" + line.strip())
         return self.match_rules[_class] + url_line
 
     def update(self):
-        pass
 
-    def toggle(self):
-        if not self.content:
-            return self.content
+        content_start = next_no_blank(self.content)
+        if self.content[content_start] != self.default_start[0]:
+            print("didn't find a content to update")
+
+        real_content_start = next_no_blank(self.content[content_start + 2:]) + content_start + 1
+        real_content_end = next_blank(self.content[real_content_start + 1:]) + real_content_start + 1
+
+        body_start = next_no_blank(self.content[real_content_end + 1:]) + real_content_end + 1
+
+        body = self.content[body_start:]
+        contents = self._toggle(body)
+        contents.extend(body)
+        return contents
+
+    def generate(self):
+        contents = self._toggle(self.content)
+        contents.extend(self.content)
+        return contents
+
+    def _toggle(self, contents=None):
+        if not contents:
+            print("data is empty")
+            return []
+
+        if contents[0] == self.default_start[0]:
+            print("already has a content")
+            return []
+
         _contents = self.default_start
 
         _matched = False
         try:
-            for match_line, matcher in self.parse():
+            for match_line, matcher in self.parse(contents):
                 _matched = True
                 line = self.format(match_line, matcher)
                 _contents.append(line)
         except ContentErrorException as e:
             print(str(e))
-            return self.content
+            return []
 
         if not _matched:
-            return self.content
+            return []
 
         _contents.append("\n")
-        _contents.extend(self.content)
         return _contents
 
 
 if __name__ == "__main__":
-    toggle = Toggle(filename='./test/test1.md',
-                    url_prefix='http://localhost/test.md')
-    print(toggle.toggle())
+    toggle = Toggle(filename='./README.md',
+                    url_prefix='https://github.com/Microndgt/toggle')
+    print(''.join(toggle.generate()))
 
 
 
